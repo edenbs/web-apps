ad7034accd74d79b91176b6fbcfe1e14173a07bd
noy yizchaki <noya248@gmail.com>
2018-03-01 21:56:25 +0200

    adding the google map to index

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -4,4 +4,17 @@ angular.module('classify').controller('HomeController', function($scope) {
 
     // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
+
+
+    var dataP = {lat: 31.969802, lng: 34.772795};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: dataP
+    });
+    var marker = new google.maps.Marker({
+        position: dataP,
+        map: map
+    });
+
+
 });
