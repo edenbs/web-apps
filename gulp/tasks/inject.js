5cf34678cca338b7c643d7cb23ae03dae204b17f
Eden Ben Shoshan <eden47@gmail.com>
2018-03-05 21:13:02 +0200

    chore(inject): sort injected files

--- gulp/tasks/inject.js
+++ gulp/tasks/inject.js
@@ -1,10 +1,11 @@
 import inject from 'gulp-inject';
+import sort from 'gulp-sort';
 import config from '../config';
 
 export default gulp => {
     gulp.task('inject:css', () => {
         return gulp.src('client/index.html')
-            .pipe(inject(gulp.src(config.paths.client.css, {read: false}), {
+            .pipe(inject(gulp.src(config.paths.client.css, {read: false}).pipe(sort()), {
                 transform: filePath => {
                     filePath = filePath.replace('/client/', '');
                     filePath = filePath.replace('/.tmp/', '');
@@ -18,7 +19,7 @@ export default gulp => {
 
     gulp.task('inject:js', () => {
         return gulp.src('client/index.html')
-            .pipe(inject(gulp.src(config.paths.client.js, {read: false}), {
+            .pipe(inject(gulp.src(config.paths.client.js, {read: false}).pipe(sort()), {
                 transform: filePath => {
                     filePath = filePath.replace('/client/', '');
                     return `<script src="${filePath}"></script>`;
