1f5c8f89d761ed4b5f58c7d635b268fd259ad20c
Eden Ben Shoshan <eden47@gmail.com>
2018-03-05 17:00:23 +0200

    feat(facebook): add facebook's share button

--- client/app/shell/toolbar/toolbar.controller.js
+++ client/app/shell/toolbar/toolbar.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('classify')
-    .controller('ToolbarController', function ($scope, $mdSidenav, $mdComponentRegistry) {
+    .controller('ToolbarController', function ($scope, $mdSidenav, $mdComponentRegistry, $window) {
         $scope.isNavIconOpened = false;
 
         $scope.$watch(function () {
@@ -14,4 +14,34 @@ angular.module('classify')
         $scope.toggleMenu = function () {
             $mdSidenav('sideNav').toggle();
         };
+
+        // Load Facebook SDK for JavaScript
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/all.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+        $window.fbAsyncInit = function() {
+            FB.init({
+                appId: '192249178206425',
+                status: true,
+                cookie: true,
+                xfbml: true,
+                version: 'v2.12'
+            });
+            console.log('I GOT CALLED');
+        };
+
+        $scope.share = function () {
+            FB.ui({
+                method: 'share',
+                display: 'popup',
+                quote: 'Hey, check me out on http://classify.com!',
+                href: 'http://classify.com'
+            }, function (response) { console.log(response)},
+            function(err) { console.log(err) });
+        }
     });
