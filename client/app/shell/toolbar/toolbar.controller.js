e8382cba934ccb72e2ff30ce1674f2b3a83d7d27
Eden Ben Shoshan <eden47@gmail.com>
2018-03-05 21:03:16 +0200

    feat(weather): add weather to header with jquery

--- client/app/shell/toolbar/toolbar.controller.js
+++ client/app/shell/toolbar/toolbar.controller.js
@@ -14,6 +14,14 @@ angular.module('classify')
         $scope.toggleMenu = function () {
             $mdSidenav('sideNav').toggle();
         };
+        
+        function convertToC(temp) {
+            return (parseFloat(temp) - 32) * (5 / 9);
+        }
+
+        $.get("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition.text%2Citem.condition.temp%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22gan%20yavne%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", function(data) {
+            $('#weather .temp').text(Math.ceil(convertToC(data.query.results.channel.item.condition.temp)) + '° ' + data.query.results.channel.item.condition.text);
+        });
 
         // Load Facebook SDK for JavaScript
         (function(d, s, id){
