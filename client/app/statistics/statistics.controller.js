96c1c98d21f2152afb85ed87856a8b93cd5c28b8
noy248 <noya248@gmail.com>
2018-03-07 13:34:44 +0200

    adding statistics graphs

--- client/app/statistics/statistics.controller.js
+++ client/app/statistics/statistics.controller.js
@@ -1,35 +1,12 @@
 'use strict';
 
-angular.module('classify').controller('StatisticsController',function($scope) {
-    $scope.classData = [{
-        "class": "A-1", "grade": "80"},
-        {"class": "A-2",
-            "grade": "90"},
-        {"class": "A-3",
-            "grade": "30"},
-        {"class": "B-1",
-            "grade": "98"},
-        {"class": "B-2",
-            "grade": "40"},
-        {"class": "B-3",
-            "grade": "90"},
-        {"class": "G-1",
-            "grade": "100"},
-        {"class": "G-2",
-            "grade": "60"},
-        {"class": "G-3",
-            "grade": "85"
-        }];
-    $scope.barData = [{"grades":"0-20", "amount":8},
-        {"grades":"21-55", "amount":10},
-        {"grades":"56-75", "amount":19},
-        {"grades":"76-90", "amount":20},
-        {"grades":"91-100", "amount":16}];
-
+angular.module('classify').controller('StatisticsController',function($scope,$stats,classSize,avgGrade,$mdToast) {
+    $scope.classSizeData = classSize;
+    $scope.avgGradeData = avgGrade;
 })
 .directive('barsChart', function ($parse) {
-        var directiveDefinitionObject =
-            {
+
+        var directiveDefinitionObject = {
                 restrict: 'E',
                 replace: false,
                 scope: {data: '=chartData' },
@@ -41,17 +18,17 @@ angular.module('classify').controller('StatisticsController',function($scope) {
                         .enter().append("div")
                         .transition().ease(d3.easeLinear)
                         .style("width", function (d) {
-                            return d.grade + "%";
+                            return d.count + "%";
                         })
                         .text(function (d) {
-                            return d.class;
+                            return d._id;
                         });
                 }
             };
         return directiveDefinitionObject;
-    }) .directive('linechart', function ($parse) {
-    var directiveDefinitionObject =
-        {
+})
+.directive('gradechart', function ($parse) {
+    var directiveDefinitionObject = {
             restrict: 'E',
             replace: false,
             scope: {data: '=chartData' },
@@ -63,10 +40,10 @@ angular.module('classify').controller('StatisticsController',function($scope) {
                     .enter().append("div")
                     .transition().ease(d3.easeLinear)
                     .style("width", function (d) {
-                        return d.amount + "%";
+                        return d.averageGrade + "%";
                     })
                     .text(function (d) {
-                        return d.grades ;
+                        return d._id ;
                     });
             }
         };
--- client/app/statistics/statistics.js
+++ client/app/statistics/statistics.js
@@ -1,12 +1,28 @@
-/**
- * Created by Noy Yizchaki on 3/5/2018.
- */
 angular.module('classify')
     .config(function ($stateProvider) {
         $stateProvider.state('shell.statistics',{
                 url: '/',
                 templateUrl: 'app/statistics/statistics.html',
-                controller: 'StatisticsController'
+                controller: 'StatisticsController',
+
+                resolve:{
+                    classSize: function ($stats) {
+                        var coun =  $stats.numInClass().$promise.then(function (data) {
+                            var all = 0;
+                            angular.forEach(data,function (d) {
+                                all += d.count;});
+
+                            angular.forEach(data,function (d) {
+                                 d.count =  (d.count / all)*100 ;});
+
+                            return data;
+                        });
+                        return coun;
+                    },
+                    avgGrade: function ($stats) {
+                        return $stats.avgGradeInClass().$promise;
+                    }
+                 }
             }
         );
     });
